49d23ea290faae89787a79556552bcd11c2e2c9b
Loverboy <diadool@list.ru>
2020-12-09 20:31:04 +0500

    Update postfix.js

--- postfix.js
+++ postfix.js
@@ -1,6 +1,6 @@
+let expression = "(1*2*3+4^5-6/7)/8*(9+10)";
 let sighs = { "+" : 1, "-" : 1, "*" : 2, "/" : 2, "^" : 3, "(" : 0}
 let stack = [];
-let expression = "(1*2*3+4^5-6/7)/8*(9+10)";
 let str = "";
 for (i = 0; i < expression.length; i++)
 {
